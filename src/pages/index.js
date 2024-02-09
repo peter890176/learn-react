482e3484b52f1c8dc52bf76c5b45f9f2a40a6175
PeterLi <peterli@PeterLis-MacBook-Air.local>
2024-02-08 20:24:06 -0500

    02082024

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,19 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import Mycomp from './qcomps/firstcomp.js'
+import Bio from './qcomps/bios.js'
+import TodoList from './qcomps/todos.js'
+import Profile from './components/profile_props.js'
+import Gallery from './qcomps/gallery_props.js'
+import Holder from './components/square.js'
+
+import PackingList from './qcomps/props_item.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <PackingList />        
     </div>
   )
 }
